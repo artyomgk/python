b2e8eccf7d5ba27981b43d9617ee72aff531a73b
KALMYKOV <rrrva43@gmail.com>
2021-12-08 21:43:28 +0300

    Initial commit

---
+++ 1_1.py
@@ -0,0 +1,16 @@
+a = 1
+print(a)
+
+b = 1.1
+print(b)
+
+c = "Привет"
+print(c)
+
+a = int(input("Введи свое целое число a: "))
+b = float(input("Введи свое вещественное число b: "))
+c = str(input("Введи свое выражение c: "))
+
+print(a)
+print(b)
+print(c)
